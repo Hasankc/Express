4f12b34dd60363b436110fbc29af5295aa92dbae
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-03-16 13:28:45 +0200

    Add errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,1 +1,29 @@
 // error handler middleware here
+
+import { Request, Response, NextFunction } from 'express'
+
+type ErrorType = {
+        message: string;
+        code: number;
+
+}
+
+
+const apiErrorHandler = (error: ErrorType, req: Request, res: Response, next: NextFunction) => {
+    
+
+    if (error.code >= 400 && error.code < 500) {
+
+        res.status(error.code).json({error: error.message})
+
+    } else if (error.code >= 500) {
+        console.error(error)
+
+    } else {
+
+        next()
+    }
+
+ }
+
+    export default apiErrorHandler
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,1 +1,2 @@
 // logger middleware here
+
