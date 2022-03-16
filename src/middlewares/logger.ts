762e679c8ba930f99a9b74aa958ff44115d5aba3
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-03-16 13:42:53 +0200

    Add logger

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,2 +1,20 @@
 // logger middleware here
 
+import {Request, Response, NextFunction } from 'express';
+
+import * as fs from 'fs';
+
+const logPath = ".logs/logs.text"
+
+function logRequest(req: Request, res: Response, next:NextFunction) {
+
+        const line = `${new DataTransfer().toLocaleString()}: ${req.method} ${req.path}\n`
+        fs.appendFile(logPath, line, (err) => {
+            if(err) throw new Error('database failed to connect');
+
+        })
+        next()
+
+}
+
+export default logRequest
